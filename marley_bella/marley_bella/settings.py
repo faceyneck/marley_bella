05a15770c97587c695861f866377b1d7d314f57a
faceyneck <whiteymccracker@mail.ru>
2022-08-01 16:56:58 -0600

    Added bootstrap, CSS, About and navbar

--- marley_bella/marley_bella/settings.py
+++ marley_bella/marley_bella/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'marley_bella_blog.apps.WebsiteConfig',
+    'marley_bella_blog.apps.MarleyBellaBlogConfig',
 ]
 
 MIDDLEWARE = [
--- marley_bella/marley_bella/urls.py
+++ marley_bella/marley_bella/urls.py
@@ -1,3 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('marley_bella_blog.urls'))
+]
+
+
+
+#Probably not needed. Boiler plate Django jank:
 """marley_bella URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +25,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
